59ec9433b8b6d3edb25b19e056c5e0d62e222832
Jennifer Liu <jenniferliu@Jennifers-MacBook-Pro.local>
2018-07-06 11:58:38 -0700

    new file

---
+++ factorial.java
@@ -0,0 +1,17 @@
+package InterviewQuestions;
+
+public class factorial {
+	public static void main(String[] args) {
+		
+	System.out.println("factorial of 6: " + factorial(6));
+}
+	public static int factorial (int number)
+	{
+		if (number == 0) {
+			return 1;
+		} else {
+			return number * factorial(number - 1);
+		}
+				
+	}
+}
